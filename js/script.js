26da01c1aee5dd9446cc1aaccad28269285501ea
AlexCode-Br <alexandre.magnos.br@gmail.com>
2025-08-19 17:29:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -912,7 +912,7 @@ document.addEventListener('DOMContentLoaded', function() {
             iconContainer.innerHTML = ''; textContainer.textContent = '';
             loaderOverlay.classList.add('show', 'loading');
 
-            const scriptURL = 'https://script.google.com/macros/s/AKfycbyEGs8Ar0T7_1X6HVt77adGPAUFJyv3WM0oH6oEv8W8qLU3QV1tqjIKv5uHj5S0wbGc/exec';
+            const scriptURL = 'https://script.google.com/macros/s/AKfycbzDUCvzIXHIUFV5sCf2wYOewHV03MujKhGzpmxeGHX_zZtsadQL83gWjkILKOcYKHek/exec';
 
             const formData = new FormData(form);
 
